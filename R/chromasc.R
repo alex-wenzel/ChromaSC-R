bf1d75dd545c10ad6a143ac822f1049b96609d07
atwenzel <atwenzel@eng.ucsd.edu>
2019-09-04 21:20:10 +0000

    rewrote run_enrichment to do per-cell enrichment w/o traj knowledge

--- R/chromasc.R
+++ R/chromasc.R
@@ -1,25 +1,45 @@
+get_es <- function(cell, gene_set) {
+  transcriptome <- names(cell)
+  all_genes_in_cell <- names(cell[cell>0])
+  gs_genes_in_cell <- base::intersect(gene_set, all_genes_in_cell)
+  gs_genes_frac <- length(gs_genes_in_cell)/length(all_genes_in_cell)
+  return(gs_genes_frac)
+}
+
+get_z_es <- function(cell_name, seurat_obj, gene_set, null_params) {
+  cell <- r14@data[,cell_name]
+  cell_size <- as.character(length(names(cell[cell>0])))
+  gene_set_size <- as.character(length(gene_set))
+
+  escore <- get_es(cell, gene_set)
+
+  if(!cell_size %in% row.names(null_params$null_means)) {
+    return(0)
+  }
+
+  nm <- null_params$null_means[cell_size, gene_set_size]
+  nv <- null_params$null_vars[cell_size, gene_set_size]
+
+  z_escore <- (escore-nm)/sqrt(nv)
+  return(z_escore)
+}
+
 #' run_enrichment
 #'
 #' @description Hi
 #'
 #' @param seurat_obj A Seurat object
-#' @param traj_vecs A list of vectors of continuous values named by a subset of seurat_obj@cell.names
-#' @param gmt_path A path to a GMT file with one or more gene sets
-#' @param window_size The fraction of pseudotime to use for the sliding window
-#' @param step_size The fraction of pseudotime values to move the sliding window on each iteration
+#' @param gene_sets A gene set object loaded by `load_gene_sets()`
 
-run_enrichment <- function(seurat_obj, traj_vecs, gmt_path, window_size, step_size) {
-  ## Load gene sets
-  gene_sets_full <- GSA::GSA.read.gmt(gmt_path)
-  gene_sets <- gene_sets_full$genesets
-  names(gene_sets) <- gene_sets_full$geneset.names
-
-  ## Make sure trajectory vectors are sorted
-  traj_vecs <- lapply(traj_vecs, function(l) l[order(l)])
-
-  ## Build a list of empty results data.frames
-  all_res <- replicate(length(traj_vecs), data.frame(row.names=names(gene_sets)))
-
-  ## Calculate the null distribution (means and vars for gs and cell sizes)
-
+run_enrichment <- function(seurat_obj, gene_sets, null_params) {
+  return(
+    pbsapply(
+      seurat_obj@cell.names,
+      function(cell.name) sapply(
+        names(gene_sets),
+        function(gs_name) get_z_es(cell.name, gene_set=gene_sets[[gs_name]],
+                                   seurat_obj=seurat_obj, null_params=null_params)
+      ), simplify=T
+    )
+  )
 }
